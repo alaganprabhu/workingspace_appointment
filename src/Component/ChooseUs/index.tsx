bdb75284c3bcbaaa584216e282b308b60b9629bc
ThamilAlagan <114654142+alaganprabhu@users.noreply.github.com>
2024-09-01 23:03:49 +0530

    update & remove cmnts

--- src/Component/ChooseUs/index.tsx
+++ src/Component/ChooseUs/index.tsx
@@ -9,36 +9,10 @@ export default function WhyChooseUs() {
         <div>
           <h2 className="mb-4">Why Choose Us?</h2>
           <div className="row row-cols-1 row-cols-sm-4">
-            {/* {text.map((feature, index) => (
-              <div key={index} className="col d-flex align-items-center">
-                <div className="card">
-                  <img
-                    src={feature.icon}
-                    className="me-2"
-                    style={{ width: "24px", height: "24px" }}
-                  />
-
-                  <span>{feature.text}</span>
-                </div>
-              </div>
-            ))} */}
-            {/* {text.map((feature, index) => (
-              <div key={index} className="col">
-                <div className="card d-flex align-items-center">
-                  <img
-                    src={feature.icon}
-                    className="me-2"
-                    style={{ width: "24px", height: "24px" }}
-                    alt={feature.text}
-                  />
-                  <span>{feature.text}</span>
-                </div>
-              </div>
-            ))} */}
+           
             {text.map((feature, index) => (
               <div key={index} className="col">
                 <div className="d-flex align-items-center">
-                  {/* Removed .card class for web view */}
                   <img
                     src={feature.icon}
                     className="me-2"
--- src/Store/store.tsx
+++ src/Store/store.tsx
@@ -1,15 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// // import workingSpaceSlice from "./apiReducer.tsx";
-
-// const store = configureStore({
-//   reducer: {
-//     workingSpace: workingSpaceSlice,
-//   },
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-// export default store;
 
 import { configureStore } from "@reduxjs/toolkit";
 import spaceReducer from "./apiReducer.tsx";
